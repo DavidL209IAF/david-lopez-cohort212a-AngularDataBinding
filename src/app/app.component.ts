c93da9e6981967a4031f742a8c74d57858563b1f
DavidL209IAF <naem209480@gmail.com>
2021-06-13 13:52:46 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'david-lopez-cohort212A-angular-data-binding';
+  firstName: string = '';
+  lastName: string = '';
+  email: string = '';
+  phone: string = '';
+  patientNo: string ='';
+  userList: any= [
+    {
+      patientno: 2233,
+      firstname: 'David',
+      lastname: 'Lopez',
+      email: 'fakeemail@test.com',
+      phone: 111111111
+    },
+
+    {
+      patientno: 2234,
+      firstname: 'John',
+      lastname: 'Doe',
+      email: 'jdoe@test.com',
+      phone:2222222222
+    }
+  ];
+
+  addUser(){
+    this.userList.push({patientno: Math.floor(Math.random() * 10000), firstname : this.firstName, lastname: this.lastName, 
+      email: this.email, phone: this.phone});
+  
+      this.firstName = '';
+      this.lastName = '';
+      this.email = '';
+      this.phone = '';
+  }
+
+  removeUser(i: any){
+    console.log(i);
+    this.userList.splice(i,1);
+  }
+  
 }

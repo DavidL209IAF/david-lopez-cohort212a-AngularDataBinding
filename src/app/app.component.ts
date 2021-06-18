356be6f7a78b228a1400043fe173ca518131d74e
DavidL209IAF <naem209480@gmail.com>
2021-06-17 17:53:05 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,43 +6,18 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'david-lopez-cohort212A-angular-data-binding';
-  firstName: string = '';
-  lastName: string = '';
-  email: string = '';
-  phone: string = '';
-  patientNo: string ='';
-  userList: any= [
-    {
-      patientno: 2233,
-      firstname: 'David',
-      lastname: 'Lopez',
-      email: 'fakeemail@test.com',
-      phone: 111111111
-    },
+  loggedIn: string = ''; //used on app.component html <p>
+  userName: string =''; //binded and passed to patients.component as createdBy
 
-    {
-      patientno: 2234,
-      firstname: 'John',
-      lastname: 'Doe',
-      email: 'jdoe@test.com',
-      phone:2222222222
-    }
-  ];
-
-  addUser(){
-    this.userList.push({patientno: Math.floor(Math.random() * 10000), firstname : this.firstName, lastname: this.lastName, 
-      email: this.email, phone: this.phone});
-  
-      this.firstName = '';
-      this.lastName = '';
-      this.email = '';
-      this.phone = '';
+  loginButton(){
+    this.loggedIn = this.userName;
   }
 
-  removeUser(i: any){
-    console.log(i);
-    this.userList.splice(i,1);
+  alertPatientAdded(){
+    alert("Patient Successfully Added.")
   }
-  
+
+  alertPatientRemoved(){
+    alert("Patient Successfully Removed.")
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { PatientsComponent } from './patients/patients.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    PatientsComponent,
+    PatientsComponent
   ],
   imports: [
     BrowserModule,
